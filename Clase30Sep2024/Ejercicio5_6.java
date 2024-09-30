178eb79bf00b52a81ab5440bbc517ba53665fe54
JSebas9427 <112889897+JSebas9427@users.noreply.github.com>
2024-09-30 20:36:38 +0000

    Ejercicio 5_6 finalizado

--- Clase30Sep2024/Ejercicio5_6.java
+++ Clase30Sep2024/Ejercicio5_6.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Ejercicio5_6 {
     public static void main(String[] args) {
-        System.out.println("Mi primer programa JAVA en hub");
+        Scanner leer = new Scanner(System.in);
+        int aprobados= 0;
+        int reprobados= 0;
+
+        System.out.print("Cantidad de estudiantes: ");
+        int cant = leer.nextInt();
+        
+        System.out.print("Ingrese las notas: ");
+        for (int i = 1; i <=cant ; i++) {
+            double notas = leer.nextDouble();
+            if (notas >= 5) {
+                aprobados++;
+            }else{
+                reprobados++;
+            }
+        }
+        System.out.println("Total estudiantes: " + cant + "\nTotal aprobados: " + aprobados + "\nTotal reprobados: " + reprobados);
     }
 }
